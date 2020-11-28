d99ec34b974eb553d1b7cd7dc86bbc43e3b6273a
Ashutosh Garg <ashutoshg@iitbhilai.ac.in>
2020-11-28 05:32:54 +0530

    commented

--- lat.py
+++ lat.py
@@ -1,34 +1,40 @@
-#to compute the special product of the two numbers in bitwise manner, i.e here compute mask
+# to compute the special product of the two numbers in bitwise manner, i.e here compute mask
 def mask(mask_bit, x):
-	output = 0
-	while mask_bit > 0 and x > 0:
-		temp = int(mask_bit % 2) * int(x % 2) #Product of ith bit  of mask and ith bit of x
-		output = output ^ int(temp) #xor with the product of same ith bit of mask and ith bit of x
-		mask_bit /= 2
-		x /= 2
-	return output
+    output = 0
+    while mask_bit > 0 and x > 0:
+        # Product of ith bit  of mask and ith bit of x
+        temp = int(mask_bit % 2) * int(x % 2)
+        # xor with the product of same ith bit of mask and ith bit of x
+        output = output ^ int(temp)
+        mask_bit /= 2
+        x /= 2
+    return output
 
-#To compute bias given alpha and beta for the SBOX	
+# To compute bias given alpha and beta for the SBOX
+
+
 def compute(alpha, beta):
-	count  = 0
-	for x in range(16):
-		if mask(alpha,x) == mask(beta,sbox[x]):
-			count += 1  #incrementing counter for matching condition of LHS and RHS
-	return count - 8
+    count = 0
+    for x in range(16):
+        if mask(alpha, x) == mask(beta, sbox[x]):
+            count += 1  # incrementing counter for matching condition of LHS and RHS
+    return count - 8
 
-sbox = [12, 10, 13, 3, 14, 11, 15, 7, 9, 8, 1, 5, 0, 2, 4, 6]#SBOX from previos assignment
-lat = [[0] * 16 for _ in range(16)] #2-D array for Linear Approximation Table
+
+sbox = [12, 10, 13, 3, 14, 11, 15, 7, 9, 8, 1, 5,
+        0, 2, 4, 6]  # SBOX from previos assignment
+lat = [[0] * 16 for _ in range(16)]  # 2-D array for Linear Approximation Table
 
 for alpha in range(16):
-	for beta in range(16):
-		lat[alpha][beta] = compute(alpha, beta) #For each pair (alpha and beta) ranging from 0-15 calculate its biasing and store 
+    for beta in range(16):
+        # For each pair (alpha and beta) ranging from 0-15 calculate its biasing and store
+        lat[alpha][beta] = compute(alpha, beta)
 
-#Printing LAT TABLE
+# Printing LAT TABLE
 for alpha in range(16):
-	for beta in range(16):
-		print(lat[alpha][beta], end ="\t") # displaying elements of the LAT
-	print()
-
+    for beta in range(16):
+        print(lat[alpha][beta], end="\t")  # displaying elements of the LAT
+    print()
 
 
 # This code can be used for Latex
--- loong.py
+++ loong.py
@@ -1,29 +1,37 @@
+# Round constants for Loong
 constants = [0x01, 0x03, 0x07, 0x0F, 0x1F, 0x3E, 0x3D, 0x3B, 0x37, 0x2F, 0x1E,
-			0x3C, 0x39, 0x33, 0x27, 0x0E, 0x1D, 0x3A, 0x35, 0x2B, 0x16, 0x2C,
-			0x18, 0x30, 0x21, 0x02, 0x05, 0x0B, 0x17, 0x2E, 0x1C, 0x38, 0x31]
+             0x3C, 0x39, 0x33, 0x27, 0x0E, 0x1D, 0x3A, 0x35, 0x2B, 0x16, 0x2C,
+             0x18, 0x30, 0x21, 0x02, 0x05, 0x0B, 0x17, 0x2E, 0x1C, 0x38, 0x31]
+
+# Function to add round key in the state
 
 
 def addRoundKey(state, round_key, const):
-	matrix = [[0] * 4 for _ in range(4)]
-	rc=[]
-	for i in range(6):
-		rc.append(const%2)
-		const = int(const/2)
-	a = rc[5] or rc[4] or rc[3]
-	b = rc[2] or rc[1] or rc[0]
-	round_constant = [[0, 0, 0, a], [0, 0, 1, b], [0, 0, 2, a], [0, 0, 4, b]]
-	for i in range(4):
-		for j in range(4):
-			matrix[i][j] = state[i][j] ^ round_key[i][j] ^ round_constant[i][j]
-	return matrix
+    matrix = [[0] * 4 for _ in range(4)]
+    rc = []
+    for i in range(6):
+        rc.append(const % 2)
+        const = int(const/2)
+    a = rc[5] or rc[4] or rc[3]
+    b = rc[2] or rc[1] or rc[0]
+    round_constant = [[0, 0, 0, a], [0, 0, 1, b], [0, 0, 2, a], [0, 0, 4, b]]
+    for i in range(4):
+        for j in range(4):
+            matrix[i][j] = state[i][j] ^ round_key[i][j] ^ round_constant[i][j]
+    return matrix
+# Function to substitute each bit according to sbox in the cell
+
 
 def subcell(state):
-	matrix = [[0] * 4 for _ in range(4)]
-	sbox = [0x0C, 0x0A, 0x0D, 0x03, 0x0E, 0x0B, 0x0F, 0x07, 0x09, 0x08, 0x01, 0x05,0x00, 0x02, 0x04, 0x06]
-	for i in range(4):
-		for j in range(4):
-			matrix[i][j] = sbox[state[i][j]]
-	return matrix
+    matrix = [[0] * 4 for _ in range(4)]
+    sbox = [0x0C, 0x0A, 0x0D, 0x03, 0x0E, 0x0B, 0x0F, 0x07,
+            0x09, 0x08, 0x01, 0x05, 0x00, 0x02, 0x04, 0x06]
+    for i in range(4):
+        for j in range(4):
+            matrix[i][j] = sbox[state[i][j]]
+    return matrix
+# Function to give multiplication of a,b in G(2^3)
+
 
 def galoisMultiplication(a, b):
     ans = 0
@@ -36,129 +44,148 @@ def galoisMultiplication(a, b):
         if check == 8:
             a ^= 3
     return ans % 16
+# Function to mix row of the state
+
 
 def mixRow(state):
-	matrix = [[0] * 4 for _ in range(4)]
-	mat = [[1, 4, 9, 13], [4, 1, 13, 9], [9, 13, 1, 4], [13, 9, 4, 1]]
-	for i in range(4):
-		for j in range(4):
-			temp = 0
-			for k in range(4):
-				temp = temp ^ galoisMultiplication(state[i][k], mat[k][j])
-			matrix[i][j] = temp
-	return matrix
+    matrix = [[0] * 4 for _ in range(4)]
+    mat = [[1, 4, 9, 13], [4, 1, 13, 9], [9, 13, 1, 4], [13, 9, 4, 1]]
+    for i in range(4):
+        for j in range(4):
+            temp = 0
+            for k in range(4):
+                temp = temp ^ galoisMultiplication(state[i][k], mat[k][j])
+            matrix[i][j] = temp
+    return matrix
+# Function to mix column of the state
+
 
 def mixColumn(state):
-	matrix = [[0] * 4 for _ in range(4)]
-	mat = [ [13, 9, 4, 1], [9, 13, 1, 4], [4, 1, 13, 9], [1, 4, 9, 13]]
-	for i in range(4):
-		for j in range(4):
-			temp = 0
-			for k in range(4):
-				temp = temp ^ galoisMultiplication(mat[i][k], state[k][j])
-			matrix[i][j] = temp
-	return matrix
+    matrix = [[0] * 4 for _ in range(4)]
+    mat = [[13, 9, 4, 1], [9, 13, 1, 4], [4, 1, 13, 9], [1, 4, 9, 13]]
+    for i in range(4):
+        for j in range(4):
+            temp = 0
+            for k in range(4):
+                temp = temp ^ galoisMultiplication(mat[i][k], state[k][j])
+            matrix[i][j] = temp
+    return matrix
+# function to get cipher from the state
+
 
 def cipher_from_state(state):
-	cipher = ""
-	for i in range(4):
-		for x in state[i]:
-			cipher += "{:01x}".format(x)
-	return cipher
+    cipher = ""
+    for i in range(4):
+        for x in state[i]:
+            cipher += "{:01x}".format(x)
+    return cipher
+
+# function for loong-64
 
 
 def algo_64bit(text, key, rc):
-	state = [[0] * 4 for _ in range(4)]
-	round_key = [[0] * 4 for _ in range(4)]
-	for i in range(4):
-		for j in range(4):
-			state[i][j] = int(text[4*i+j], 16)
-			round_key[i][j] = int(key[4*i+j], 16)
-	state = addRoundKey(state, round_key, rc[0])
-	for i in range(1, 17):
-		state = subcell(state)
-		state = mixRow(state)
-		state = mixColumn(state)
-		state = subcell(state)
-		state = addRoundKey(state, round_key, rc[i])
-	ciphertext = cipher_from_state(state)
-	return ciphertext
+    state = [[0] * 4 for _ in range(4)]
+    round_key = [[0] * 4 for _ in range(4)]
+    for i in range(4):
+        for j in range(4):
+            state[i][j] = int(text[4*i+j], 16)
+            round_key[i][j] = int(key[4*i+j], 16)
+    state = addRoundKey(state, round_key, rc[0])
+    for i in range(1, 17):
+        state = subcell(state)
+        state = mixRow(state)
+        state = mixColumn(state)
+        state = subcell(state)
+        state = addRoundKey(state, round_key, rc[i])
+    ciphertext = cipher_from_state(state)
+    return ciphertext
+
+# function for loong-80
+
 
 def algo_80bit(text, key, rc):
-	state = [[0] * 4 for _ in range(4)]
-	round_key0 = [[0] * 4 for _ in range(4)]
-	round_key1 = [[0] * 4 for _ in range(4)]
-	for i in range(4):
-		for j in range(4):
-			state[i][j] = int(text[4*i+j], 16)
-			round_key0[i][j] = int(key[4*i+j], 16)
-			if i == 0:
-				round_key1[i][j] = int(key[16+j], 16)
-			else:
-				round_key1[i][j] = int(key[4*(i-1)+j], 16)
-	state = addRoundKey(state, round_key0, rc[0])
-	for i in range(1, 21):
-		state = subcell(state)
-		state = mixRow(state)
-		state = mixColumn(state)
-		state = subcell(state)
-		if i % 2:
-			state = addRoundKey(state, round_key1, rc[i])
-		else:
-			state = addRoundKey(state, round_key0, rc[i])
-	ciphertext = cipher_from_state(state)
-	return ciphertext
+    state = [[0] * 4 for _ in range(4)]
+    round_key0 = [[0] * 4 for _ in range(4)]
+    round_key1 = [[0] * 4 for _ in range(4)]
+    for i in range(4):
+        for j in range(4):
+            state[i][j] = int(text[4*i+j], 16)
+            round_key0[i][j] = int(key[4*i+j], 16)
+            if i == 0:
+                round_key1[i][j] = int(key[16+j], 16)
+            else:
+                round_key1[i][j] = int(key[4*(i-1)+j], 16)
+    state = addRoundKey(state, round_key0, rc[0])
+    for i in range(1, 21):
+        state = subcell(state)
+        state = mixRow(state)
+        state = mixColumn(state)
+        state = subcell(state)
+        if i % 2:
+            state = addRoundKey(state, round_key1, rc[i])
+        else:
+            state = addRoundKey(state, round_key0, rc[i])
+    ciphertext = cipher_from_state(state)
+    return ciphertext
+
+# function for loong-128
+
 
 def algo_128bit(text, key, rc):
-	state = [[0] * 4 for _ in range(4)]
-	round_key0 = [[0] * 4 for _ in range(4)]
-	round_key1 = [[0] * 4 for _ in range(4)]
-	for i in range(4):
-		for j in range(4):
-			state[i][j] = int(text[4*i+j], 16)
-			round_key0[i][j] = int(key[4*i+j], 16)
-			round_key1[i][j] = int(key[16+4*i+j], 16)
-	state = addRoundKey(state, round_key0, rc[0])
-	for i in range(1, 33):
-		state = subcell(state)
-		state = mixRow(state)
-		state = mixColumn(state)
-		state = subcell(state)
-		if i % 2:
-			state = addRoundKey(state, round_key1, rc[i])
-		else:
-			state = addRoundKey(state, round_key0, rc[i])
-	ciphertext = cipher_from_state(state)
-	return ciphertext
+    state = [[0] * 4 for _ in range(4)]
+    round_key0 = [[0] * 4 for _ in range(4)]
+    round_key1 = [[0] * 4 for _ in range(4)]
+    for i in range(4):
+        for j in range(4):
+            state[i][j] = int(text[4*i+j], 16)
+            round_key0[i][j] = int(key[4*i+j], 16)
+            round_key1[i][j] = int(key[16+4*i+j], 16)
+    state = addRoundKey(state, round_key0, rc[0])
+    for i in range(1, 33):
+        state = subcell(state)
+        state = mixRow(state)
+        state = mixColumn(state)
+        state = subcell(state)
+        if i % 2:
+            state = addRoundKey(state, round_key1, rc[i])
+        else:
+            state = addRoundKey(state, round_key0, rc[i])
+    ciphertext = cipher_from_state(state)
+    return ciphertext
+
+# Function to encrypt the plaintext
 
 
 def encryption(n, plaintext, key):
-	if n == 64 and len(plaintext) == 16 and len(key) == 16:
-		cipher = algo_64bit(plaintext, key, constants)
-	elif n == 80 and len(plaintext) == 16 and len(key) == 20:
-		cipher = algo_80bit(plaintext, key, constants)
-	elif n == 128 and len(plaintext) == 16 and len(key) == 32:
-		cipher = algo_128bit(plaintext, key, constants)
-	else:
-		return "Wrong Input" 
-	return cipher
+    if n == 64 and len(plaintext) == 16 and len(key) == 16:
+        cipher = algo_64bit(plaintext, key, constants)
+    elif n == 80 and len(plaintext) == 16 and len(key) == 20:
+        cipher = algo_80bit(plaintext, key, constants)
+    elif n == 128 and len(plaintext) == 16 and len(key) == 32:
+        cipher = algo_128bit(plaintext, key, constants)
+    else:
+        return "Wrong Input"
+    return cipher
+
+# Function to decrypt the plaintext
 
 
 def decryption(n, cipher, key):
-	if n == 64 and len(cipher) == 16 and len(key) == 16:
-		reverse_constant = constants[0:17][::-1]
-		plaintext = algo_64bit(cipher, key, reverse_constant)
-	elif n == 80 and len(cipher) == 16 and len(key) == 20:
-		reverse_constant = constants[0:21][::-1]
-		plaintext = algo_80bit(cipher, key, reverse_constant)
-	elif n == 128 and len(cipher) == 16 and len(key) == 32:
-		reverse_constant = constants[0:33][::-1]
-		plaintext = algo_128bit(cipher, key, reverse_constant)
-	else:
-		return "Wrong Input" 
-	return plaintext
+    if n == 64 and len(cipher) == 16 and len(key) == 16:
+        reverse_constant = constants[0:17][::-1]
+        plaintext = algo_64bit(cipher, key, reverse_constant)
+    elif n == 80 and len(cipher) == 16 and len(key) == 20:
+        reverse_constant = constants[0:21][::-1]
+        plaintext = algo_80bit(cipher, key, reverse_constant)
+    elif n == 128 and len(cipher) == 16 and len(key) == 32:
+        reverse_constant = constants[0:33][::-1]
+        plaintext = algo_128bit(cipher, key, reverse_constant)
+    else:
+        return "Wrong Input"
+    return plaintext
 
 
+# Driver code
 n = 128
 plaintext = "0000000000000000"
 key = "00000000000000000000000000000000"
@@ -168,7 +195,6 @@ print(ciphertext)
 text = decryption(n, ciphertext, key)
 print(text)
 print()
-
 
 
 n = 80
@@ -182,7 +208,6 @@ print(text)
 print()
 
 
-
 n = 64
 plaintext = "0000000000000000"
 key = "0000000000000000"
@@ -192,5 +217,3 @@ print(ciphertext)
 text = decryption(n, ciphertext, key)
 print(text)
 print()
-
-
